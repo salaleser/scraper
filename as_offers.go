c443faa17b743658010898d21d220df7a5e1f29d
salaleser <salaleser@users.noreply.github.com>
2020-05-21 11:43:53 +0300

    Fix Price data type

--- as_offers.go
+++ as_offers.go
@@ -4,7 +4,7 @@ type Offers []struct {
 	ActionText     ActionText `json:"actionText"`
 	Type           string     `json:"type"`
 	PriceFormatted string     `json:"priceFormatted"`
-	Price          int        `json:"price"`
+	Price          float32    `json:"price"`
 	BuyParams      string     `json:"buyParams"`
 	Version        Version    `json:"version"`
 	Assets         Assets     `json:"assets"`
